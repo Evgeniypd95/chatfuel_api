06eeaa076a05217227cc796af6ceac59fda9ff8b
Evgeny Pavlov <evgeniypd95@mail.ru>
2020-01-26 17:44:51 +0000

    terms and conditions

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>LSE Random Coffee Terms and Conditions</title>
 
 	<style type="text/css">
 
@@ -68,22 +68,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>LSE Random Coffee Terms and Conditions</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+	<section>
+    <div class="ti-terms ti-section ti-short-intro ng-scope" translate="usTerms.body"><p><span>TERMS OF USE</span></p><p><span>Welcome to LSE Random Coffee.</span></p><strong id="eligibility">1. Eligibility.</strong><p><span>You must be at least 18 years of age to signup on LSE Random Coffee and use the Service. By creating an account and using the Service, you represent and warrant that:</span></p><ul><li><span>you are not a person who is barred from using the Service under the laws of the United States or any other applicable jurisdiction–meaning that you do not appear on the U.S. Treasury Department’s list of Specially Designated Nationals or face any other similar prohibition,</span></li><li><span>you will comply with this Agreement and all applicable local, state, national and international laws, rules and regulations, and</span></li><li><span>you have never been convicted of or pled no contest to a felony, a sex crime, or any crime involving violence, and that you are not required to register as a sex offender with any state, federal or local sex offender registry.</span></li></ul><strong id="your-account">2. Your Account.</strong><p><span>In order to use LSE Random Coffee, you must sign in using your Facebook login. &nbsp;If you do so, you authorize us to access and use certain Facebook account information, including but not limited to your public Facebook profile and information about Facebook friends you share in common with other LSE Random Coffee users.</span><span>.</span></p><strong id="safety">3. Safety; Your Interactions with Other Users.</strong><p><span>LSE Random Coffee&nbsp;</span><strong>is not responsible for the conduct of any user on or off of the Service. You agree to use caution in all interactions with other users, particularly if you decide to communicate off the Service or meet in person. In addition, you agree to review and follow Tinder’s </strong><a href="/safety"><strong>Safety Tips</strong> </a><strong>prior to using the Service. You agree that you will not provide your financial information (for example, your credit card or bank account information), or wire or otherwise send money, to other users</strong> <span>.</span></p><p><strong>YOU ARE SOLELY RESPONSIBLE FOR YOUR INTERACTIONS WITH OTHER USERS. YOU UNDERSTAND THAT LSE Random Coffee DOES NOT&nbsp;CONDUCT CRIMINAL BACKGROUND CHECKS ON ITS USERS OR OTHERWISE INQUIRE INTO THE BACKGROUND OF ITS USERS. LSE Random Coffee MAKES NO REPRESENTATIONS OR WARRANTIES AS TO THE CONDUCT OF USERS.</strong> </p><strong id="community-rules">4. Community Rules.</strong><p><span><br></span><span>By using the Service, you agree that you will not:</span></p><ul><li><span>use the Service for any purpose that is illegal or prohibited by this Agreement.</span></li><li><span>use the Service for any harmful or nefarious purpose.</span></li><li><span>use the Service in order to damage LSE Random Coffee.</span></li><li><span>violate our </span><a href="/community-guidelines?locale=en"><span>Community Guidelines</span> </a><span>, as updated from time to time.</span></li><li><span>spam, solicit money from or defraud any users.</span></li><li><span>impersonate any person or entity or post any images of another person without his or her permission.</span></li><li><span>bully, “stalk,” intimidate, assault, harass, mistreat or defame any person.</span></li><li><span>post any Content that violates or infringes anyone’s rights, including rights of publicity, privacy, copyright, trademark or other intellectual property or contract right.</span></li><li><span>post any Content that is hate speech, threatening, sexually explicit or pornographic; incites violence; or contains nudity or graphic or gratuitous violence.</span></li><li><span>post any Content that promotes racism, bigotry, hatred or physical harm of any kind against any group or individual.</span></li><li><span>solicit passwords for any purpose, or personal identifying information for commercial or unlawful purposes from other users or disseminate another person’s personal information without his or her permission.</span></li><li><span>use another user’s account, share an account with another user, or maintain more than one account.</span></li><li><span>create another account if we have already terminated your account, unless you have our permission.</span></li></ul><p><span>LSE Random Coffee reserves the right to investigate and/ or terminate your account without a refund of any purchases if you have violated this Agreement, misused the Service or behaved in a way that LSE Random Coffee regards as inappropriate or unlawful, including actions or communications that occur on or off the Service.</span></p><strong id="disclaimers">5. Disclaimers.</strong><p><span>LSE Random Coffee GRANTS NO WARRANTIES OF ANY KIND, WHETHER EXPRESS, IMPLIED, STATUTORY OR OTHERWISE WITH RESPECT TO THE SERVICE (INCLUDING ALL CONTENT CONTAINED THEREIN), INCLUDING, WITHOUT LIMITATION, ANY IMPLIED WARRANTIES OF SATISFACTORY QUALITY, MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT. LSE Random Coffee DOES NOT REPRESENT OR WARRANT THAT (A) THE SERVICE WILL BE UNINTERRUPTED, SECURE OR ERROR FREE, (B) ANY DEFECTS OR ERRORS IN THE SERVICE WILL BE CORRECTED, OR (C) THAT ANY CONTENT OR INFORMATION YOU OBTAIN ON OR THROUGH THE SERVICE WILL BE ACCURATE.</span></p><p><span>LSE Random Coffee TAKES NO RESPONSIBILITY FOR ANY CONTENT THAT YOU OR ANOTHER USER OR THIRD PARTY POSTS, SENDS OR RECEIVES THROUGH THE SERVICE. ANY MATERIAL DOWNLOADED OR OTHERWISE OBTAINED THROUGH THE USE OF THE SERVICE IS ACCESSED AT YOUR OWN DISCRETION AND RISK.</span></p><p><span>LSE Random Coffee DISCLAIMS AND TAKES NO RESPONSIBILITY FOR ANY CONDUCT OF YOU OR ANY OTHER USER, ON OR OFF THE SERVICE.</span></p><strong id="limitation-of-liability">6. Limitation of Liability.</strong><p><span>TO THE FULLEST EXTENT PERMITTED BY APPLICABLE LAW, IN NO EVENT SHALL LSE Random Coffee, ITS AFFILIATES, EMPLOYEES, LICENSORS OR SERVICE PROVIDERS BE LIABLE FOR ANY INDIRECT, CONSEQUENTIAL, EXEMPLARY, INCIDENTAL, SPECIAL, PUNITIVE, OR ENHANCED DAMAGES, INCLUDING, WITHOUT LIMITATION, LOSS OF PROFITS, WHETHER INCURRED DIRECTLY OR INDIRECTLY, OR ANY LOSS OF DATA, USE, GOODWILL, OR OTHER INTANGIBLE LOSSES, RESULTING FROM: (I) YOUR ACCESS TO OR USE OF OR INABILITY TO ACCESS OR USE THE SERVICE; (II) THE CONDUCT OR CONTENT OF OTHER USERS OR THIRD PARTIES ON, THROUGH OR FOLLOWING USE OF THE SERVICE; OR (III) UNAUTHORIZED ACCESS, USE OR ALTERATION OF YOUR CONTENT, EVEN IF LSE Random Coffee HAS BEEN ADVISED AT ANY TIME OF THE POSSIBILITY OF SUCH DAMAGES.</span></p><p><span>THE LIMITATION OF LIABILITY PROVISIONS SET FORTH IN THIS SECTION 6 SHALL APPLY EVEN IF YOUR REMEDIES UNDER THIS AGREEMENT FAIL WITH RESPECT TO THEIR ESSENTIAL PURPOSE.</span></p><p></p></div>
+  </section>
 	</div>
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-	<?php 
+	<!-- <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p> -->
+	<!-- <?php 
 
 	$array1 = array(15,16,17,18,19,20,21,22,23);
 	$array2 = $array1;
@@ -95,7 +89,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	?>
 	<pre><?php print_r($pairs); ?></pre>
 
-	<?php echo $user_item->title;?>
+	<?php echo $user_item->title;?> -->
 </div>
 
 </body>
